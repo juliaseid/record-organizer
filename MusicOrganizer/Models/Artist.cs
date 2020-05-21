33d25374d287fa605eec0305657a77e1983d66b1
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-21 15:29:58 -0600

    fix views

--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -4,36 +4,36 @@ namespace MusicOrganizer.Models
 {
   public class Artist
   {
-    public string FirstName {get; set;}
-    public string LastName{get;set;}
-    public string FullName{ get;set; }
-    public List<Record> Discography = new List<Record>{};
-    public int Id {get;}
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName { get; set; }
+    public List<Record> Discography = new List<Record> { };
+    public int Id { get; }
     private static int _currentId = 0;
-    private static List<Artist> _instances = new List<Artist> {};
+    private static List<Artist> _instances = new List<Artist> { };
 
-    public Artist(string first, string last)
-    {
-      FirstName = first;
-      LastName=last;
-      FullName= $"{first} {last}";
-      _currentId++;
-      Id= _currentId;
-      _instances.Add(this);
-    }
     public Artist(string first)
     {
       FirstName = first;
-      FullName= first;
+      FullName = first;
       _currentId++;
-      Id= _currentId;
+      Id = _currentId;
+      _instances.Add(this);
+    }
+    public Artist(string first, string last)
+    {
+      FirstName = first;
+      LastName = last;
+      FullName = $"{first} {last}";
+      _currentId++;
+      Id = _currentId;
       _instances.Add(this);
     }
     public void AddRecord(Record record)
     {
       Discography.Add(record);
     }
-    
+
 
   }
 }
--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -4,30 +4,36 @@ namespace MusicOrganizer.Models
 {
   public class Record
   {
-    public string Name {get; set;}
-    public int Id {get;}
-    public Artist ThisArtist {get; set;}
+    public string Name { get; set; }
+    public int Id { get; }
+    public Artist ThisArtist { get; set; }
     private static int _currentId = 0;
-    private static List<Record> _instances = new List<Record> {};
-  
-  public Record (string name, Artist artist )
-  {
-    Name = name;
-    ThisArtist = artist;
-    _currentId++;
-    Id = _currentId;
-    _instances.Add(this);
-  }
-  public Record (string name)
-  {
-    Name = name;
-   // ThisArtist = new Artist("Unknown");
-    _currentId++;
-    Id = _currentId;
-    _instances.Add(this);
-  }
+    private static List<Record> _instances = new List<Record> { };
+
+    public Record(string name, Artist artist)
+    {
+      Name = name;
+      ThisArtist = artist;
+      _currentId++;
+      Id = _currentId;
+      _instances.Add(this);
+    }
+
+    public static List<Record> GetAll()
+    {
+      return _instances;
+    }
+
+    // public Record (string name)
+    // {
+    //   Name = name;
+    //  // ThisArtist = new Artist("Unknown");
+    //   _currentId++;
+    //   Id = _currentId;
+    //   _instances.Add(this);
+    // }
 
   }
 
-  
+
 }
